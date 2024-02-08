4286448ce4aad59cfbc734d36b49aceb461c8319
Sneha <adhikarisneha01@gmail.com>
2024-02-08 12:30:40 +0545

    playlist added

--- lib/projects/3. Music Player/data/model/music_player_model.dart
+++ lib/projects/3. Music Player/data/model/music_player_model.dart
@@ -11,5 +11,18 @@ class MusicPlayerModel {
 
   List<MusicPlayerModel> musicList = [
   MusicPlayerModel(musicName: "Someone Like You", musicSinger: 'Adele', musicPath: "assets/music_player_assets/music/Adele_-_Someone_Like_You.mp3"),
-  MusicPlayerModel(musicName: "Million Years Ago", musicSinger: "Adele", musicPath: "assets/music_player_assets/music/Adele___Million_Years_Ago_Lyric.mp3")
+  MusicPlayerModel(musicName: "Million Years Ago", musicSinger: "Adele", musicPath: "assets/music_player_assets/music/Adele___Million_Years_Ago_Lyric.mp3"),
+  MusicPlayerModel(musicName: "Bimbaakash-Khai", musicSinger: "Bartika Eam Rai", musicPath: "assets/music_player_assets/music/Bimbaakash-Khai_[Lyrical_Video].mp3"),
+  MusicPlayerModel(musicName: "WildFire (Dadhelo)", musicSinger: "Bipul Chettri", musicPath: "assets/music_player_assets/music/Bipul_Chettri_-_Wildfire_(Dadhelo)(1).mp3"),
+  MusicPlayerModel(musicName: "Thinking Out Loud", musicSinger: "Ed Sheeran", musicPath: "assets/music_player_assets/music/Ed_Sheeran_-_Thinking_Out_Loud_[Official_Video].mp3"),
+  MusicPlayerModel(musicName: "Jaise Mera Tu", musicSinger: "***", musicPath: "assets/music_player_assets/music/Jaise_Mera_Tu.mp3"),
+  MusicPlayerModel(musicName: "One Thing", musicSinger: "One Direction", musicPath: "assets/music_player_assets/music/One_Direction_-_One_Thing_(Official_Video).mp3"),
+  MusicPlayerModel(musicName: "You & I", musicSinger: "One Direction", musicPath: "assets/music_player_assets/music/One_Direction_-_You_&_I_(Official_Video).mp3"),
+  MusicPlayerModel(musicName: "Style", musicSinger: "Taylor Swift", musicPath: "assets/music_player_assets/music/Taylor+Swift+-+Style.mp3"),
+  MusicPlayerModel(musicName: "Begin Again", musicSinger: "Taylor Swift", musicPath: "assets/music_player_assets/music/Taylor_Swift_-_Begin_Again.mp3"),
+  MusicPlayerModel(musicName: "Tear Drops on My Guitar", musicSinger: "Taylor Swift", musicPath: "assets/music_player_assets/music/Taylor_Swift_-_Teardrops_On_My_Guitar.mp3"),
+  MusicPlayerModel(musicName: "Wildest Dreams", musicSinger: "Taylor Swift", musicPath: "assets/music_player_assets/music/Taylor_Swift_-_Wildest_Dreams.mp3"),
+  MusicPlayerModel(musicName: "Tera Woh Pyar", musicSinger: "***", musicPath: "assets/music_player_assets/music/Tera_Woh_Pyar_(Nawazishein_Karam),_Momina_Mustehsan_&_Asim_Azhar,_Episode_6,_Coke_Studio_Season_9.mp3"),
+  MusicPlayerModel(musicName: "Tose Naina", musicSinger: "Arijit Singh", musicPath: "assets/music_player_assets/music/Tose_Naina.mp3"),
+  MusicPlayerModel(musicName: "Yaad Ma Rakhnechhu", musicSinger: "Swoopna Suman", musicPath: "assets/music_player_assets/music/Yaad_Ma_Rakhnechhu_-_Official_Lyrical_video_-_Swoopna_Suman.mp3"),
   ];
